4ce1693a18dc17c0123c8c25b7b9cc9a579d71d6
Jonas <mail@jonasgoebel.net>
2025-05-11 15:25:08 +0200

    adding presentation content

--- js/titleChanger.js
+++ js/titleChanger.js
@@ -7,15 +7,13 @@ const updatePageTitle = (pageIndex) => {
         footerName = ''
         // references = '[5,6] [7]'
     }
-    // if (pageIndex >= 4) {
-    //     pageTitle = ''
-    //     // references = '[5,6] [7]'
-    // }
-    // if (pageIndex >= 4) pageTitle = 'Inhalt 3'
+    if (pageIndex >= 3) references = '[1]'
+    if (pageIndex >= 5) references = '[2] [3] [4]'
+    if (pageIndex >= 6) references = '[5]'
     // if (pageIndex >= 10) pageTitle = 'Fazit'
     // if (pageIndex === 11) pageTitle = ''
     // if (pageIndex >= 12) pageTitle = 'Quellen'
-    if (pageIndex >= 9) {
+    if (pageIndex >= 8) {
         pageTitle = ""
         footerName = ""
     }
