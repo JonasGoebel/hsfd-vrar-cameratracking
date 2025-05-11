4808c0ed94900aceb8a7fee43029050ae923ea35
Jonas <mail@jonasgoebel.net>
2025-05-11 22:57:36 +0200

    linked img references, fixed img aspect ratio

--- js/titleChanger.js
+++ js/titleChanger.js
@@ -4,16 +4,17 @@ const updatePageTitle = (pageIndex) => {
     let footerName = ''
     if (pageIndex >= 1) {
         pageTitle = 'Kameratracking'
-        footerName = ''
-        // references = '[5,6] [7]'
+        footerName = 'Jonas Göbel, Omar Schure'
     }
     if (pageIndex >= 3) references = '[1]'
     if (pageIndex >= 5) references = '[2] [3] [4]'
     if (pageIndex >= 6) references = '[5]'
-    // if (pageIndex >= 10) pageTitle = 'Fazit'
-    // if (pageIndex === 11) pageTitle = ''
-    // if (pageIndex >= 12) pageTitle = 'Quellen'
-    if (pageIndex >= 8) {
+    if (pageIndex >= 7) references = '[6]'
+    if (pageIndex >= 8) references = '[8]'
+    if (pageIndex >= 9) references = '[7]'
+    if (pageIndex >= 10) references = ''
+    if (pageIndex == 11) references = '[9]'
+    if (pageIndex >= 12) {
         pageTitle = ""
         footerName = ""
     }
